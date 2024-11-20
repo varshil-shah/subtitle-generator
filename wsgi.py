e9fd563d13e75585b6c30a5703960b052c965642
Varshil Shah <varshilshah.in@gmail.com>
2024-11-20 22:23:23 +0530

    Change port number from 8080 to 8000

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ import os
 load_dotenv()
 
 if __name__ == "__main__":
-    port = int(os.getenv('PORT', 8080))
+    port = int(os.getenv('PORT', 8000))
     print(f"Starting Waitress server on port {port}...")
     serve(
         app,
